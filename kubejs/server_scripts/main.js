193f9dc1c7f7473514194a368b09ce136ab0b503
ky7969 <88926833+ky7969@users.noreply.github.com>
2026-05-28 20:20:33 -0500

    Modify recipes and tooltips for hoppers

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -1,12 +1,37 @@
 ServerEvents.recipes(event => {
     event.recipes.create.mixing(
-    [Item.of("farmersdelight:fried_rice")],
-    [
-        'farmersdelight:rice',
-        'minecraft:carrot',
-        'farmersdelight:onion',
-        'minecraft:bowl',
-        'minecraft:egg'
-    ]
-    ).heated()
+        [Item.of("farmersdelight:fried_rice")],
+        [
+            'farmersdelight:rice',
+            'minecraft:carrot',
+            'farmersdelight:onion',
+            'minecraft:bowl',
+            'minecraft:egg'
+        ]
+        ).heated()
+    event.remove(
+        { input: '#minecraft:planks', output:'woodenhopper:wooden_hopper' }
+    )
+    event.replaceOutput(
+        { output: 'minecraft:hopper' },
+        'minecraft:hopper',
+        'woodenhopper:wooden_hopper'
+    )
+    event.replaceInput(
+        { input: 'minecraft:hopper' },
+        'minecraft:hopper',
+        'woodenhopper:wooden_hopper'
+    )
+    event.shapeless(
+        Item.of('woodenhopper:wooden_hopper'),
+        'minecraft:hopper'
+    )
+})
+ItemEvents.modifyTooltips(event => {
+    event.add(['minecraft:hopper'],Text.red("Lags the server, use wooden hopper instead. Exchange for wooden hopper in crafting grid."))
+})
+ItemEvents.canPickUp('minecraft:hopper', event => {
+    event.player.give('woodenhopper:wooden_hopper'),
+    event.itemEntity.remove(),
+    event.cancel()
 })
